b45864ec2e5425880863cd88a77dcec200e9194e
syc <syc@partdb.com>
2022-04-07 15:11:28 +0900

    Add: 자바스크립트 인라인 @tabutabu98

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -120,6 +120,14 @@ public class BasicController {
         return "basic/block";
     }
 
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+
+        model.addAttribute("user", new User("User\"A\"", 10));
+        addUsers(model);
+        return "basic/javascript";
+    }
+
     private void addUsers(Model model) {
         List<User> list = new ArrayList<>();
         list.add(new User("UserA", 10));
